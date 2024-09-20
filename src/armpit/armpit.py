ef124dc263f6d4158451d7fc35a99ae1732e248c
Kent Slaney <kent@slaney.org>
2024-09-20 15:46:21 -0700

    reload updated modules and packaging options

--- src/armpit/armpit.py
+++ src/armpit/armpit.py
@@ -5,17 +5,28 @@ def armpit():
     path = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(path, "update.py")
 
+    bind, primary, secondary, packaged = sys.argv[1]
+    bind, packaged = int(bind, 16), int(packaged, 16)
+
     exec(compile(open(path).read(), "update.py", "exec"), globals())
     class armpit(armpit):
-        pass
+        name = "__main__" if packaged & 8 else None
 
-    bind = int(sys.argv[1])
-    for module in sys.argv[2:]:
+    if packaged & 4:
+        armpit.flat_path()
+    if packaged & 2:
+        if packaged & 1:
+            armpit.set_package(sys.argv[2])
+        else:
+            armpit.flat_package()
+
+    for module in sys.argv[2 + (packaged & 1):]:
         armpit(module)
-    
+
     if bind & 1:
-        armpit.bind('\C-h', 'current')
+        armpit.bind(r'\C-' + primary, 'current', bind & 4)
     if bind & 2:
-        armpit.bind('\C-o', 'rerun')
+        key = secondary if bind & 1 else primary
+        armpit.bind(r'\C-' + key, 'rerun', bind & 8)
 
 armpit()
